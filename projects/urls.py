e483578b20e43ef706af25a26fe162ee60b55285
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2024-06-29 12:08:03 +0600

    Task update

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetail, ProjectCreate
+from .views import ProjectListView, ProjectDetail, ProjectCreate, ProjectUpdate
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('project/<int:pk>/', ProjectDetail.as_view(), name='project_detail'),
     path('project-create/', ProjectCreate.as_view(), name='project_create'),
+    path('project-update/<int:pk>', ProjectUpdate.as_view(), name='project_update'),
     # path('', views.project_list, name='project_list'),
     # path('project/new/', views.project_create, name='project_create'),
     # path('project/<int:pk>/edit/', views.project_edit, name='project_edit'),
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView 
-from django.views.generic import DetailView 
-from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView  
 from django.urls import reverse_lazy
 from .models import Projects
 
@@ -21,6 +19,16 @@ class ProjectDetail(DetailView):
     template_name = 'projects/project_detail.html'
 
 class ProjectCreate(CreateView):
+    model = Projects
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('project_list')
+    template_name = 'projects/project_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+class ProjectUpdate(UpdateView):
     model = Projects
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('project_list')
